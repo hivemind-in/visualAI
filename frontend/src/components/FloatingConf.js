e5a37afc9cb85a95df6b9011fdaee73df0c466cb
amit <amit.jha6700@gmail.com>
2020-04-25 19:03:30 +0530

    floating fun. added

--- frontend/src/components/FloatingConf.js
+++ frontend/src/components/FloatingConf.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import {ic_drag_handle} from 'react-icons-kit/md/ic_drag_handle'
+import { ic_drag_handle } from 'react-icons-kit/md/ic_drag_handle'
 import Icon from 'react-icons-kit'
-
+import Draggable from 'react-draggable'
 function FloatingConf(props) {
-    return(
-        <div className={"floating-conf-menu"}>
-            <div className={"dragger"}>
-                <Icon size={30} icon={ic_drag_handle} />
+    return (
+        <Draggable 
+        bounds={"parent"}
+        handle={".dragger"} 
+        axis={"x"}>
+            <div className={"floating-conf-menu"}>
+                <div className={"dragger"}>
+                    <Icon size={30} icon={ic_drag_handle} />
+                </div>
             </div>
-        </div>
+        </Draggable>
     )
 }
 
--- frontend/src/components/FloatingMenu.js
+++ frontend/src/components/FloatingMenu.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import Search from './Search'
-import {ic_drag_handle} from 'react-icons-kit/md/ic_drag_handle'
+import { ic_drag_handle } from 'react-icons-kit/md/ic_drag_handle'
 import Icon from 'react-icons-kit'
+import Draggable from 'react-draggable'
 function FloatingMenu(props) {
-    return(
-        <div className={"floating-menu-main"}>
-            <div className={"dragger"}>
-                <Icon size={30} icon={ic_drag_handle} />
+    return (
+        <Draggable 
+        bounds={"parent"}
+        handle={".dragger"}>
+            <div className={"floating-menu-main"}>
+                <div className={"dragger"}>
+                    <Icon size={30} icon={ic_drag_handle} />
+                </div>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <Search />
             </div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <Search />
-        </div>
+        </Draggable>
     )
 }
 
