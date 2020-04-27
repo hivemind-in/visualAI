9d01a72a8b9cb080a3388df027c986dfff7de5f8
Wyverns010 <wyverns@gihub.com>
2020-04-27 14:55:36 +0530

    server side

--- backend/socket.py
+++ backend/socket.py
@@ -1,1 +1,17 @@
 import os
+import zerorpc
+
+class Ping(object):
+    def ping(self):
+        return "receiving data from ip=%s at port=%s" % self.ip,self.port
+
+class Endpoint(object):
+
+    def __init__(self,type_con,ip,port):
+        self.type_con = type_con
+        self.ip = ip
+        self.port = port
+        self.server = zerorpc.Server(Ping())
+        self.server.bind(type_con+"://"+ip+":"+port)
+        self.server.run()
+
